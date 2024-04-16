1b6e1a73e67beaa39a404fe94018c8bf735bd532
Aaron Mahlke <aaron.mahlke@gmail.com>
2024-04-16 21:16:11 +0200

    Update clear color

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -6,10 +6,7 @@ var target = Vector2()
 const SPEED = 10.0
 
 func _physics_process(delta):
-	# Look at the player
 	target = player.global_position
 	global_position = lerp(global_position, target, delta * SPEED)
-	# Move towards the player
-	pass
 
 
